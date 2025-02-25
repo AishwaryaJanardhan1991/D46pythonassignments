d8001d773ec78f15d4b6a6410e5cd873f9b80503
AishwaryaJanardhan1991 <shivaishwarya14@gmail.com>
2025-02-25 14:02:56 +0530

    Delete DollarToPound.py

--- DollarToPound.py
+++
@@ -1,6 +0,0 @@
-#Excercise 10
-#Converting amount in pound to dollar(Same also attached with notepad document with file name)
-Pounds = float(input('enter an amount in pounds:(£)'))
-Dollar_conversion_rate = 1.26
-Dollars = Pounds * Dollar_conversion_rate 
-print(f"£{Pounds} are ${Dollars}")
